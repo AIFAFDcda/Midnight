a044951ff3051fb29e039c6bfc657c0d659b89bc
AIFAFDcda <99197421+AIFAFDcda@users.noreply.github.com>
2022-05-10 22:21:19 +0200

    Update club.lua

--- club.lua
+++ club.lua
@@ -6,11 +6,10 @@
 require("lib/natives")
 
 function mpx()
-	return "MP" .. script_global:new(1574907):get_int64() .. "_"
+	return "MP" .. script_global:new(1574915):get_int64() .. "_"
 end
 
 system.fiber(function()
-    local player_index = script_global:new(1574907):get_int64()
     STATS.STAT_SET_INT(string.smart_joaat(mpx() .. "CLUB_POPULARITY"), 1000)
     utils.notify("Nightclub Popularity", "Successfully changed your club popularity to max", 28, 1)
 end)
